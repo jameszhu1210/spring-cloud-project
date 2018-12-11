324966bb72e08afd3375b96795fff8d408d0f209
jzhu1837 <Zhu.James@corp.sysco.com>
2018-12-11 11:49:43 +0800

    add frdt ws

---
+++ frdt-ws/src/main/java/com/sysco/FrdtWsApplication.java
@@ -0,0 +1,12 @@
+package com.sysco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrdtWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrdtWsApplication.class, args);
+	}
+}
