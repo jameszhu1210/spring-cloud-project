353474d2293bdcb94c298982193745c39d62236a
jzhu1837 <Zhu.James@corp.sysco.com>
2018-12-17 10:36:26 +0800

    add gateway zuul

---
+++ cop-ws/src/main/java/com/sysco/CopWsApplication.java
@@ -0,0 +1,14 @@
+package com.sysco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopWsApplication.class, args);
+	}
+
+}
+
